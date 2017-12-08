2936d5b1bf37e75e74732a6a240ed6a0e8b655f8
= <geophilus8@gmail.com>
2017-12-08 11:20:54 +0530

    Articles listing created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,9 @@ class ArticlesController < ApplicationController
 	def new
 		@article = Article.new
 	end
-
+	def index
+		@article = Article.all
+	end
 	def create
 		@article = Article.new(article_params)
 		if @article.save
@@ -14,6 +16,18 @@ class ArticlesController < ApplicationController
 	end
 	def show
 		@article = Article.find(params[:id])
+	end
+	def edit
+		@article = Article.find(params[:id])
+	end
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice] = "Article was updated successfully"
+			redirect_to article_path(@article)
+		else	
+			render 'edit'
+		end
 	end
 	private
 	def article_params
