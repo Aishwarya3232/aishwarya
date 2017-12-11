daaf8d9587311b3132d22402259ffb8678d97008
= <geophilus8@gmail.com>
2017-12-11 12:42:21 +0530

    Added signup page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'page#home'
   get 'about', to: 'page#about'
-
+  get 'signup', to: 'users#new'
   resources :articles
+  resources :users, except: [:new]
 end
